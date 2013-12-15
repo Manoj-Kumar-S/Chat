fe461ec4a8e44c9db9516619f8df08fd0c8934ed
Sandeep Dasika <dasika.sandy@gmail.com>
2013-12-15 23:22:11 +0530

    Changed directory structure and integrated setuptools.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-Chat
-====
+PyChat
+======
 
-Chat application in python
+A lightweight chat client written purely in Python using the Twisted networking engine.
+
+Setup Instructions
+------------------
+
+Clone the repository, and then...
+.. code-block:: bash
+
+    $ python setup.py install
+
+
+Demo
+----
+
+After installation, set up the server-
+.. code-block:: bash 
+
+    $ python server.py
+
+To start the client on a terminal-
+.. code-block:: bash 
+
+    $ python client.py
+
+To-do
+----
+
+A lot more...
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+      name = 'pychat',
+      version = '1.0.1',
+      author = 'Sandeep Dasika',
+      author_email = 'dasika.sandy@gmail.com',
+      description = 'A simple lightweight chat client',
+      license = open('LICENSE.txt').read(),
+      keywords = 'chat python',
+      url = 'https://github.com/gravetii/Chat',
+      packages = ['Chat',],
+      long_description = open('README.md').read(),
+      install_requires = ['twisted==13.2.0',],
+      zip_safe = False,
+      )
